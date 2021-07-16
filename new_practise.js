63a3d71b83e5284ef783c1286db6eb4a96f191fb
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-07-16 23:56:55 +0600

    huge

---
+++ new_practise.js
@@ -0,0 +1,9 @@
+var myName = "dipraj";
+myName = 55222;
+console.log(myName);
+function sumation(a) {
+    return a;
+}
+console.log(sumation(5));
+var student, _a = void 0,  = _a.name,  = _a["Dipraj"],  = _a.number,  = _a[865555];
+console.log(student);
---
+++ new_practise.ts
@@ -0,0 +1,25 @@
+type infro = string | number;
+
+let myName:infro = "dipraj";
+myName = 55222;
+console.log(myName);
+
+function sumation(a:number):number {
+    return a;
+
+}
+console.log(sumation(5));
+
+interface dataInfo {
+    name:string;
+    number:number;
+    marks?:number;
+}
+let student:dataInfo {
+    name:"Dipraj",
+    number: 865555,
+
+
+
+}
+console.log(student);
